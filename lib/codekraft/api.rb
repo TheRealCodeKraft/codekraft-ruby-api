acdbbe982e0581f43ef965b5cf9e7452615d01fd
Arnaud DRAZEK <adrazek@gmail.com>
2017-09-26 20:46:09 +0000

    Trying to make configuration

--- lib/codekraft/api.rb
+++ lib/codekraft/api.rb
@@ -1,6 +1,9 @@
 module Codekraft
   module Api
    require 'codekraft/api/version'
+
+   require 'codekraft/api/configuration'
+
    require 'codekraft/api/defaults'
    require 'codekraft/api/endpoint'
    require 'codekraft/api/service'
