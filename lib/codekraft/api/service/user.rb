9207950cb507722653d6b524999f83bc7030d6fd
Arnaud DRAZEK <adrazek@gmail.com>
2017-09-26 14:41:32 +0000

    Setting up user create method

--- lib/codekraft/api/service/user.rb
+++ lib/codekraft/api/service/user.rb
@@ -3,6 +3,15 @@ module Codekraft
     module Service
       class User < Base
 
+        def create params
+          params[:salt] = BCrypt::Engine.generate_salt
+          params[:encrypted_password] = encrypt_password(params[:password], params[:salt])
+          params.delete :password
+          params.delete :password_confirm
+          params[:email] = params[:email].downcase
+          super(params)
+        end
+
       end
     end
   end
