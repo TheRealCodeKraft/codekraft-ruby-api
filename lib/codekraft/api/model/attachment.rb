03cc0250499cdc2a1ec4063e2ec84f066630eeaf
Arnaud DRAZEK <no@codekraft.local>
2018-03-05 17:13:17 +0100

    Managing attachment serializer

--- lib/codekraft/api/model/attachment.rb
+++ lib/codekraft/api/model/attachment.rb
@@ -6,7 +6,7 @@ module Codekraft
 
         belongs_to :parent, polymorphic: true
 
-        has_attached_file :attachment
+        has_attached_file :attachment, :styles => { :large => "1000x1000>", :medium => "300x300>", :thumb => "100x100>" }
         do_not_validate_attachment_file_type :attachment
       end
     end
--- lib/codekraft/api/serializer/attachment_serializer.rb
+++ lib/codekraft/api/serializer/attachment_serializer.rb
@@ -8,12 +8,12 @@ module Codekraft
         def file_url
           if Rails.env.production?
             if object.attachment.url.include?("s3.amazonaws")
-              "https:#{object.attachment.url.sub("s3.amazonaws", "s3.eu-west-2.amazonaws")}"
+              "https:#{object.attachment.url(:medium).sub("s3.amazonaws", "s3.eu-west-2.amazonaws")}"
             else
-              "https://forgemeup-api.herokuapp.com#{object.attachment.url}"
+              "https://forgemeup-api.herokuapp.com#{object.attachment.url(:medium)}"
             end
           else
-            "http://localhost:3000#{object.attachment.url}"
+            "http://localhost:3000#{object.attachment.url(:medium)}"
           end
         end
 
